d58566bb4b8381b0ac5fac8caf71ebb4ac4c8429
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2024-06-14 20:47:11 +0500

    addded clerk auth

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn, SignUp } from '@clerk/nextjs'
+
+const SignInPage = () => {
+    return (
+        <div>
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInPage
